428718880d7062d4e47a67c2fc20ce7657a8cb2a
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-08-15 08:26:19 -0400

    resolved blank player names

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,16 @@
+//function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+}
+
 var playerInfo = {
-  name: window.prompt("what is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
